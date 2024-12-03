0e1dcc0ea667b2bb25a99cb08e6635d5a29a1fae
Pierre <pierre.laffaille@outlook.fr>
2024-12-03 14:33:38 +0100

    finished 01

---
+++ Cookies & localStorage/01-localStorage/script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("input-texte");
+const saveBtn = document.getElementById("sauvegarder");
+const message = document.getElementById("message");
+const loadBtn = document.getElementById("load");
+const displayStorage = document.getElementById("display-storage");
+const clearBtn = document.getElementById("clear");
+
+saveBtn.addEventListener("click", () => {
+	localStorage.setItem("nom", input.value);
+	message.textContent = "Sauvegarde effectuée";
+});
+
+loadBtn.addEventListener("click", () => {
+	const nom = localStorage.getItem("nom");
+	if (nom) {
+		displayStorage.textContent = nom;
+	} else {
+		displayStorage.textContent = "Rien dans le localStorage";
+	}
+});
+
+clearBtn.addEventListener("click", () => {
+	localStorage.clear();
+});
