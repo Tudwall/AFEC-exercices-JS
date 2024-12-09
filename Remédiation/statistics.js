c22786894a16d1473212cff89e4313c06672d876
Pierre <pierre.laffaille@outlook.fr>
2024-12-09 16:29:46 +0100

    refactored highest

--- Remédiation/statistics.js
+++ Remédiation/statistics.js
@@ -3,9 +3,10 @@ const nb = [4, 8, 15, 16, 23, 42];
 let sum = nb.reduce((a, b) => a + b, 0);
 console.log(sum);
 
-let sorted = nb.toSorted((a, b) => a - b);
-console.log(sorted);
-let max = sorted[sorted.length - 1];
+let max = nb[0];
+for (let i = 1; i < nb.length; i++) {
+	if (nb[i] > max) max = nb[i];
+}
 console.log(max);
 
 let avg = nb.reduce((a, b) => (a + b) / nb.length, 0);
@@ -13,8 +14,10 @@ console.log(avg);
 
 function calculateStats(arr) {
 	let sum = arr.reduce((a, b) => a + b, 0);
-	let sorted = arr.sort((a, b) => a - b);
-	let max = sorted[sorted.length - 1];
+	let max = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] > max) max = arr[i];
+	}
 	let avg = arr.reduce((a, b) => (a + b) / arr.length);
 
 	let result = {
