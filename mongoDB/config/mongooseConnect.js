67295e36dc372f88ce014fb782ed4ac187f8eb63
Pierre <pierre.laffaille@outlook.fr>
2025-01-17 11:54:19 +0100

    changed confirmation message

--- mongoDB/config/mongooseConnect.js
+++ mongoDB/config/mongooseConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const mongooseConnect = async () => {
 	try {
 		await mongoose.connect(process.env.URL_DATABASE);
-		console.log("bien passé");
+		console.log("mongoose is connected to database");
 	} catch (err) {
 		console.error(`mongoose connection issue ${err}`);
 		process.exit();
