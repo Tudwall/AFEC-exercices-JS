6554b8a2d5771baabcdfe00c3bdb34757cb37c6b
Pierre <pierre.laffaille@outlook.fr>
2025-01-17 11:53:53 +0100

    used mongoose-sequence to generate id

--- mongoDB/models/user.model.js
+++ mongoDB/models/user.model.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-sequence")(mongoose);
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	id: Number,
 	name: String, // String is shorthand for {type: String}
 	email: String,
 	age: Number,
 	password: String,
 	hidden: Boolean,
 });
+
+userSchema.plugin(autoIncrement, { inc_field: "id" });
 
 const userModel = mongoose.model("User", userSchema);
 
